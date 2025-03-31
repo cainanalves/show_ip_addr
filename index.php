23ba0c7683d974f686ebf73e142f7dd70746b24f
Cainan T. Alves <cainan.teixeira10@hotmail.com>
2025-03-31 10:07:41 -0300

    v1.0.1

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
-//	$mac = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/";
-//	$mac1 = file_get_contents($mac);
-//	$url = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac1/public-ipv4s";
-//	$instance_id = file_get_contents($url);
-// 	echo "<h1><br/><br/><p align='center'>IP: $instance_id </p></h1>";
+	$mac = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/";
+	$mac1 = file_get_contents($mac);
+	$url = "http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac1/public-ipv4s";
+	$instance_id = file_get_contents($url);
+	echo "<h1><br/><br/><p align='center'>IP: $instance_id </p></h1>";
 
-
-    echo "<h1><br/><br/><p align='center'> App funfando!! </p></h1>";
 ?>
 
 
